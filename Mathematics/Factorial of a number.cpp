1018a77475aa4000cffb4c5a399eaa87a26dcde3
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2022-11-04 11:17:07 +0530

    Create Factorial of a number.cpp

---
+++ Mathematics/Factorial of a number.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+long long factorial(int);
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+    public:
+        // Complete this function
+    long long factorial(int N) {
+        // Your code here
+        long long result = 1;
+        for(int i=1; i<=N; i++)
+        result = result*i;
+        return result;
+    }
+
+};
+
+//{ Driver Code Starts.
+int main() {
+    int T, N;
+    cin >> T; // input number of testcases
+    while (T--) {
+        cin >> N; // input n
+        Solution ob;
+        cout << ob.factorial(N) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
